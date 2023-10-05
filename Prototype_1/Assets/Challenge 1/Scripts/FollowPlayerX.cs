17eaa9efd52d2cd135154f8da3b59b0a70ce3eea
JosephRichmond <joseph.richmond@s.capregboces.org>
2023-10-05 09:32:58 -0400

    Challenge1_JosephRichmond

--- Prototype_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prototype_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -1,25 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowPlayerX : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject plane;
     public Vector3 offset;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    
-    void LateUpdate()
+    void Update()
     {
-        transform.position = player.transform.position + offset;
-        
-
+        transform.position = plane.transform.position + offset;
     }
 }
--- Prototype_1/Assets/Scripts/PLayerController.cs
+++ Prototype_1/Assets/Scripts/PLayerController.cs
@@ -12,18 +12,20 @@ public class PLayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
     {
-       
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on the vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Roatates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
+        
 
     }
 }
